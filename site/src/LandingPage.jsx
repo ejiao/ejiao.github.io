2344fc30958bd3bfac523035f7b6eeed8011337c
ejiao <edward_jiao@brown.edu>
2020-01-21 23:51:23 -0800

    styling landing

--- site/src/LandingPage.jsx
+++ site/src/LandingPage.jsx
@@ -3,11 +3,38 @@ import React, { Component } from 'react';
 import './landing_page.css';
 
 class LandingPage extends Component {
+
+	renderText() {
+		return (
+			<div className="landing_page__text">
+        <div className="landing_page__title">
+        	This is Eddie Jiao’s personal website.
+        </div>
+        <div className="landing_page__subtitle">
+        	<p>
+        		Eddie Jiao is a developer/designer. Eddie graduated from Brown in 2019 and is currently at Slack in San Francisco.
+        	</p>
+				</div>
+			</div>
+		);
+	}
+
+	renderNav() {
+		return(
+			<div className="nav">
+				<a href='#'>WORK</a>
+				<a href='#'>BLOG</a>
+				<a href='#'>ABOUT</a>
+			</div>
+		);
+	}
+
   render() {
     return (
       <div className="landing_page">
-        <h1> Hello World </h1>
-      </div>
+      	{this.renderNav()}
+      	{this.renderText()}
+			</div>
     );
   }
 }
