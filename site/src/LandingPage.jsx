44c93aa2774ad99ce4d6e407560a55bd5ad8fb9d
ejiao <edward_jiao@brown.edu>
2023-08-12 22:45:21 -0700

    update for Humane

--- site/src/LandingPage.jsx
+++ site/src/LandingPage.jsx
@@ -10,7 +10,7 @@ class LandingPage extends Component {
 			<div className="landing_page__text">
 				<div className="landing_page__title">
 		        	<p> Hey there, I'm Eddie Jiao. Welcome to my fun slice of the internet. </p>
-		        	<p> I live in San Francisco and build software for a living. I’m currently working on Slack, and was previously at Donut and Brown University.</p>
+		        	<p> I live in San Francisco and build software for a living. I do experimental stuff at <a href="https://hu.ma.ne/">Humane</a>, and was previously at <a href="https://slack.com/">Slack</a>, <a href="https://www.donut.com/">Donut</a> and <a href="https://www.brown.edu/">Brown University</a>.</p>
 		        	<p> Engineering + Design + 3D Graphics </p>
 		        </div>
 			</div>
