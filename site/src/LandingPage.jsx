922708f94282f5ad2d4aca0d999f6c9813b209ec
ejiao <edward_jiao@brown.edu>
2025-04-18 12:02:18 -0700

    early 2025 updates

--- site/src/LandingPage.jsx
+++ site/src/LandingPage.jsx
@@ -10,9 +10,11 @@ class LandingPage extends Component {
 			<div className="landing_page__text">
 				<div className="landing_page__title">
 		        	<p> Hey there, I'm Eddie Jiao. Welcome to my fun slice of the internet. </p>
-		        	<p> I live in San Francisco and build software for a living. I work on the future of interfaces. Previously at <a href="https://hu.ma.ne/">Humane</a>, <a href="https://slack.com/">Slack</a>, <a href="https://www.donut.com/">Donut</a> and <a href="https://www.brown.edu/">Brown University</a>.</p>
-		        	<p> HCI + Engineering + Design </p>
+		        	<p> I live in San Francisco and founded <a href="https://commonknowled.ge/">Common Knowledge</a>, where we make experimental interfaces for the next generation of documents and computers. </p>
 		        </div>
+				<div className="landing_page__subtitle">
+					<p>Previously at <a href="https://hu.ma.ne/">Humane</a>, <a href="https://slack.com/">Slack</a>, <a href="https://www.donut.com/">Donut</a> and <a href="https://www.brown.edu/">Brown University</a>.</p>
+				</div>
 			</div>
 		);
 	}
