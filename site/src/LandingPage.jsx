78afa3f20270b5b3b8e5725356c6d6afcf410386
ejiao <edward_jiao@brown.edu>
2025-04-19 11:41:03 -0700

    fix padding

--- site/src/LandingPage.jsx
+++ site/src/LandingPage.jsx
@@ -10,7 +10,7 @@ class LandingPage extends Component {
 			<div className="landing_page__text">
 				<div className="landing_page__title">
 		        	<p> Hey there, I'm Eddie Jiao. Welcome to my fun slice of the internet. </p>
-		        	<p> I live in San Francisco and founded <a href="https://commonknowled.ge/">Common Knowledge</a>, where we make experimental interfaces for the next generation of documents and computers. </p>
+		        	<p> I live in San Francisco and founded <a href="https://commonknowled.ge/">Common Knowledge</a>, building experimental interfaces for the next generation of documents and computers. </p>
 		        </div>
 				<div className="landing_page__subtitle">
 					<p>Previously at <a href="https://hu.ma.ne/">Humane</a>, <a href="https://slack.com/">Slack</a>, <a href="https://www.donut.com/">Donut</a> and <a href="https://www.brown.edu/">Brown University</a>.</p>
