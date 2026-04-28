c43ab65a5f5b54ba9537555c7f674c98b822a56b
ejiao <edward_jiao@brown.edu>
2026-04-28 00:34:51 -0700

    Tighten homepage work history copy

--- site/src/LandingPage.jsx
+++ site/src/LandingPage.jsx
@@ -19,18 +19,16 @@ class LandingPage extends Component {
             diffusion-based generative graphical user interface. About a year
             ago, I founded{' '}
             <a href="https://www.commonknowled.ge/">Common Knowledge</a>, a
-            research lab building new types of computing surfaces. Before that,
-            I was a founder fellow at{' '}
+            research lab building new types of computing surfaces. Previously, I
+            was a founder fellow at{' '}
             <a href="https://www.southparkcommons.com/">South Park Commons</a>.
-            Before that, I was a software engineer at Humane, where we tried to
-            build an AI-native computer. At Humane, I built, prototyped, and
-            shipped a few highly{' '}
+            Before that, I was an engineer at Humane, where I built a few
+            highly{' '}
             <a href="https://patents.justia.com/patent/20240345718">novel</a>{' '}
             <a href="https://patents.justia.com/patent/20250103144">
               interfaces
-            </a>.
-            Before Humane I was a software engineer at{' '}
-            <a href="https://slack.com/">Slack</a>, where I worked on the{' '}
+            </a>, and at <a href="https://slack.com/">Slack</a>, where I worked
+            on the{' '}
             <a href="https://slack.com/downloads/mac">desktop</a> and{' '}
             <a href="https://apps.apple.com/us/app/slack/id618783545">iOS</a>{' '}
             clients. I studied Computer Science at{' '}
